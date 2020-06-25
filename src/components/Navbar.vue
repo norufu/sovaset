6c91390ab2bd826c5d5446c62171629925da2258
Joshua Di Lello <jd@Joshuas-MacBook-Pro.local>
2020-06-25 18:45:00 -0400

    Added logo

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,11 @@
 <template>
   <div>
     <b-navbar class="navbar-custom shadow-sm" toggleable="lg">
-      <b-navbar-brand to="/" href="#">SS</b-navbar-brand>
+      <b-navbar-brand to="/" href="#">
+        <img class="logo" src="../../public/UI/logo.svg" />
+      </b-navbar-brand>
       <b-navbar-nav class="ml-auto">
-        <b-nav-item
-          class="navItems"
-          @click="() => $router.push('About')"
-          href="#"
-          >Contact</b-nav-item
-        >
+        <b-nav-item class="navItems" @click="() => $router.push('About')" href="#">Contact</b-nav-item>
       </b-navbar-nav>
     </b-navbar>
   </div>
@@ -17,11 +14,15 @@
 <script>
 export default {
   name: "Navbar",
-  methods: {},
+  methods: {}
 };
 </script>
 
 <style scoped>
+.logo {
+  transform: scale(1.4);
+  margin-left: 20px;
+}
 .navbar-custom {
   background-color: black;
 }
